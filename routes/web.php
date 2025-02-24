b33b0e0db354be92dc631ca85bb55c4dd0c1716a
wahyudedik <wdedyk@gmail.com>
2025-02-24 21:46:43 +0700

    new file:   app/Http/Controllers/CourseController.php 	new file:   app/Http/Controllers/PostController.php 	new file:   app/Http/Controllers/ProfileController.php 	new file:   app/Http/Controllers/UserController.php 	new file:   resources/views/courses/create.blade.php 	new file:   resources/views/courses/edit.blade.php 	new file:   resources/views/courses/index.blade.php 	new file:   resources/views/courses/show.blade.php 	new file:   resources/views/posts/create.blade.php 	new file:   resources/views/posts/edit.blade.php 	new file:   resources/views/posts/index.blade.php 	new file:   resources/views/posts/show.blade.php 	new file:   resources/views/profiles/create.blade.php 	new file:   resources/views/profiles/edit.blade.php 	new file:   resources/views/profiles/index.blade.php 	new file:   resources/views/profiles/show.blade.php 	new file:   resources/views/users/create.blade.php 	new file:   resources/views/users/edit.blade.php 	new file:   resources/views/users/index.blade.php 	new file:   resources/views/users/show.blade.php 	modified:   routes/web.php

--- routes/web.php
+++ routes/web.php
@@ -1,9 +1,15 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\PostController;
+use App\Http\Controllers\UserController;
+use App\Http\Controllers\CourseController;
+use App\Http\Controllers\ProfileController;
 
 Route::get('/', function () {
     return view('welcome');
 });
-
-
+Route::resource('/profiles', ProfileController::class);
+Route::resource('/courses', CourseController::class);
+Route::resource('/posts', PostController::class);
+Route::resource('/users', UserController::class);
